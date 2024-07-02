91413e360dc5d274713571bf5b69c63700656fae
pro1zero <jenish1097@gmail.com>
2024-07-03 04:05:11 +0530

    add all ways to find if a string has unique chars

--- ArraysAndStrings/UniqueChars.java
+++ ArraysAndStrings/UniqueChars.java
@@ -1,5 +1,61 @@
 package ArraysAndStrings;
 
-public class UniqueChars {
+import java.util.Arrays;
 
+// Ask if string is ASCII or Unicode.
+// assuming the string is only lowercase a-z as I'm using bitVector
+public class UniqueChars{
+    public static boolean hasUniqueCharsNestedLoop(String s){
+        checkStringValidity(s);
+
+        for (int i = 0; i < s.length(); i++){
+            char front = s.charAt(i);
+            for(int j = i + 1; j <s.length(); j++){
+                char rear = s.charAt(j);
+                if (front == rear) return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean hasUniqueCharsSingleLoop(String s){
+        checkStringValidity(s);
+
+        int bitVector = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            int value = s.charAt(i) - 'a';
+            if ((bitVector & (1 << value)) > 0){
+                return false;
+            }
+            bitVector |= (1 << value);
+        }
+        return true;
+    }
+
+    public static boolean hasUniqueCharsUsingSorting(String s){
+        checkStringValidity(s);
+
+        char[] array = s.toCharArray();
+        Arrays.sort(array);
+        String sortedString = new String(array);
+
+        char prev = sortedString.charAt(0);
+        int i = 1;
+        while(i < sortedString.length()){
+            char current = sortedString.charAt(i);
+            if (prev == current){
+                return false;
+            }
+            prev = current;
+            i += 1;
+        }
+        return true;
+    }
+
+    public static void checkStringValidity(String s){
+        if (s == null || s.isEmpty()){
+            throw new IllegalArgumentException("Input string cannot be null or empty");
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import ArraysAndStrings.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // 1. UniqueChars
+        System.out.println(UniqueChars.hasUniqueCharsNestedLoop("jenishj"));
+        System.out.println(UniqueChars.hasUniqueCharsSingleLoop("jenish"));
+        System.out.println(UniqueChars.hasUniqueCharsUsingSorting("jenishj"));
+
     }
 }
